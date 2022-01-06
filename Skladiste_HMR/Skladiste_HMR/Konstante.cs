ae47171bba25c8e47ae642b6a2510bf035e0893c
Josipa Radak <jradak@pmfst.hr>
2022-01-06 20:01:57 +0100

    dodana stat. klasa provjera i update klase korisnik

--- Skladiste_HMR/Skladiste_HMR/Konstante.cs
+++ Skladiste_HMR/Skladiste_HMR/Konstante.cs
@@ -10,4 +10,72 @@ namespace Skladiste_HMR
     {
         public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Skladiste;Integrated Security=True;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
     }
+    static class Provjere
+    {
+        public static bool KratkaProvjeraUnosa(string unos)
+        {
+            if (unos.Length < 3 || unos.Length > 50)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ProvjeraUnosa(string unos)
+        {
+            if (unos.Length < 3)
+            {
+                return false;
+            }
+            if (!Char.IsLetter(unos, 0) || Char.IsLower(unos, 0))
+            {
+                return false;
+            }
+            for (int i = 1; i < unos.Length; i++)
+            {
+                if (!Char.IsLetter(unos, i) || Char.IsUpper(unos, i))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        public static bool ProvjeraKorImena(string unos)
+        {
+            if (unos.Length < 3)
+            {
+                return false;
+            }
+            foreach (char znak in unos)
+            {
+                if (!Char.IsLetterOrDigit(znak) || Char.IsUpper(znak))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool ProvjeraLozinke(string unos)
+        {
+            if (unos.Length < 7)
+            {
+                return false;
+            }
+            return true;
+        }
+        public static bool ProvjeraCijene(string cijena)
+        {
+            if (cijena.Length < 1)
+            {
+                return false;
+            }
+            double pomocna = 0;
+            bool uspjeh = double.TryParse(cijena, out pomocna);
+            if (!uspjeh)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
 }
